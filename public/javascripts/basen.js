da23c8ddb77121d095ae542488c2dbf3f10975ba
haxor211 <bolo12345@gmail.com>
2018-05-30 13:43:49 +0200

    update
    update

--- public/javascripts/basen.js
+++ public/javascripts/basen.js
@@ -8,6 +8,25 @@ window.onload = function() {
         liczbaOs += parseInt($('.tab-liczba')[row].innerText)
     });
 
+    $('#czy_hotelowy').on({
+        change: function (e) {
+            if($('#hotelowy-sel')[0].selected == true) {
+                $('#numer-pokoju').removeClass('invis');
+            } else {
+                $('#numer-pokoju').addClass('invis');
+            }
+        }
+    });
+
+    $('#register').on({
+        click: function() {
+            if($('#hotelowy-sel')[0].selected == false) {                
+                $('#numer-pokoju')[0].type = 'text';
+                $('#numer-pokoju')[0].value = 'Gość Zewnętrzny';
+            }
+        }
+    })
+
     $('#liczba-os')[0].innerText = 'Liczba Osob na basenie: ' + liczbaOs;
     
     $('#addbtn').on('click', () => {
